7525cc1289a3387ee8c17f4513b2d6aa6d4183bd
Davin <davin-dj@hotmail.com>
2022-09-14 19:23:31 -0400

    Completed activity and bonus

--- public/index.js
+++ public/index.js
@@ -1,9 +1,17 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
-    let countValue = 0;
+    
+    let response = await fetch(`http://127.0.0.1:9001/counter`)
+    
+    let responseJS = await response.json()
+
+    console.log(responseJS)
+
+    let countValue = responseJS.value;
 
     function increment(){
         countValue++;
@@ -15,8 +23,14 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function refresh(){
+        countValue = responseJS.value;
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
